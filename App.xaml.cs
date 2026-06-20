bfc2dad18444b5a70ec7ff607b8d6fd56fd9e640
Willian Theodoro <business.will.theo@gmail.com>
2026-06-20 12:52:39 -0300

    Console
    Adicionando um console ao projeto

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace Fluxogrammer;
@@ -9,5 +8,19 @@ namespace Fluxogrammer;
 /// </summary>
 public partial class App : Application
 {
+    [DllImport("kernel32.dll")]
+    private static extern bool AllocConsole();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        AllocConsole();
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        AllocConsole();
+        base.OnExit(e);
+    }
 }
 
