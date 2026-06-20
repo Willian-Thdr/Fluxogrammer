7d251f4729c0986c7f85dccb835f09d07cde7525
Willian Theodoro <business.will.theo@gmail.com>
2026-06-20 12:16:49 -0300

    Segundo commit
    Adicionando primeiros botões

---
+++ Source/Classes/Commands/MenuButtonsActions.xaml.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using System.Windows.Controls;
+
+public class MenuButtonsActions
+{
+    public static void GetButtonNew(Button component)
+    {
+        component.Click += (s, e) =>
+        {
+            Console.WriteLine("Novo");
+        };
+    }
+
+    public static void GetButtonLoad(Button component)
+    {
+        component.Click += (s2, e2) =>
+        {
+            Console.WriteLine("Carregar");
+        };
+    }
+}
