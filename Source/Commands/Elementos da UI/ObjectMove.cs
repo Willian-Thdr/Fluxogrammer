1322463c6af1885894ed36f32fcc1cda4308a62a
Willian Theodoro <business.will.theo@gmail.com>
2026-06-29 20:58:16 -0300

    Correção
    Usei o projeto para aprender a fazer encapsulamento, e de proveito melhorei a leitura e compreensão do código

--- Source/Commands/Elementos da UI/ObjectMove.cs
+++ Source/Commands/Elementos da UI/ObjectMove.cs
@@ -1,99 +1,43 @@
 using System.Windows.Controls;
 using System.Windows;
-using System.Windows.Controls.Primitives;
 
 public class ObjectMove
 {
-    public static void Connect(Canvas canva, Grid bloco, TextBox txt)
+    public static void Connect(Canvas canva, CreateObject.BlocoVisual bloco)
     {
         bool press = false;
+
         Point offset = new();
 
-            bloco.PreviewMouseLeftButtonDown += (s, e) =>
+        bloco.Grid.PreviewMouseLeftButtonDown += (s, e) =>
+        {
+            FluxConnectionManager.EndConnect(canva, bloco);
+            press = true;
+
+            Point mouse = e.GetPosition(canva);
+
+            offset.X = mouse.X - Canvas.GetLeft(bloco.Grid);
+            offset.Y = mouse.Y - Canvas.GetTop(bloco.Grid);
+
+            bloco.Grid.CaptureMouse();
+        };
+
+        bloco.Grid.MouseMove += (s, e) =>
+        {
+            if (press)
             {
-                press = true;
-    
                 Point mouse = e.GetPosition(canva);
-    
-                offset.X = mouse.X - Canvas.GetLeft(bloco);
-                offset.Y = mouse.Y - Canvas.GetTop(bloco);
-    
-                bloco.CaptureMouse();
-            };
-    
-            bloco.MouseMove += (s, e) =>
-            {
-                if (press)
-                {
-                    Point mouse = e.GetPosition(canva);
-            
-                    Canvas.SetTop(bloco, mouse.Y - offset.Y);
-                    Canvas.SetLeft(bloco, mouse.X - offset.X);
-                }            
-            };  
-    
-            bloco.MouseLeftButtonUp += (s, e) => 
-            {
-                press = false;
-    
-                bloco.ReleaseMouseCapture();
-            };     
+        
+                Canvas.SetTop(bloco.Grid, mouse.Y - offset.Y);
+                Canvas.SetLeft(bloco.Grid, mouse.X - offset.X);
+            }            
+        };  
 
-            bloco.PreviewMouseRightButtonDown += (s, e) =>
-            {
-                ContextMenu menu = new();
+        bloco.Grid.MouseLeftButtonUp += (s, e) => 
+        {
+            press = false;
 
-                MenuItem rename = new MenuItem();
-                rename.Header = "Renomear";
-
-                MenuItem expandir = new MenuItem();
-                expandir.Header = "Expandir";
-
-                MenuItem recolher = new MenuItem();
-                recolher.Header = "Recolher";
-
-                MenuItem del = new MenuItem();
-                del.Header = "Deletar";
-
-                rename.Click += (s2, e2) =>
-                {
-                    txt.IsReadOnly = false;
-                    txt.Focus();                   
-                };
-
-                expandir.Click += (s2, e2) =>
-                {
-                    bloco.Height = double.NaN;
-                };
-
-                recolher.Click += (s2, e2) =>
-                {
-                    bloco.Height = 25;
-                };
-
-                del.Click += (s2, e2) =>
-                {
-                    canva.Children.Remove(bloco);
-                };
-
-                menu.Placement = PlacementMode.MousePoint;
-
-                menu.Items.Add(rename);
-
-                if (bloco.Height <= 25)
-                {
-                    menu.Items.Add(expandir);
-                    menu.Items.Remove(recolher);
-                }
-                else
-                {
-                    menu.Items.Add(recolher);
-                    menu.Items.Remove(expandir);
-                }
-
-                menu.Items.Add(del);
-
-                menu.IsOpen = true;
-            };
+            bloco.Grid.ReleaseMouseCapture();
+        };     
     }
 }
--- Source/Storage/ProjetoInfo.cs
+++ Source/Storage/ProjetoInfo.cs
@@ -1,17 +1,6 @@
-using System.Windows.Controls;
-
 public class ProjetoInfo
 {
     public string Nome { get; set; }
     public List<Objeto> objetos { get; set; } = new();
-}
-
-public class Objeto
-{
-    public string Id { get; set; }
-    public string content { get; set; }
-    public double X { get; set; }
-    public double Y { get; set; }
-    public double Wdt { get; set; }
-    public double Hegt { get; set; }
+    public List<LinhaStorage> linhass { get; set; } = new();
 }
