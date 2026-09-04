f118ab39d78d330fa1bfe777cab44639362e8f09
Willian Theodoro <business.will.theo@gmail.com>
2026-09-03 23:56:52 -0300

    bug fix
    Salvamento de linhas concertado
    
    Close #12
    Close #2

--- Source/Template/FluxTemplate.cs
+++ Source/Template/FluxTemplate.cs
@@ -3,24 +3,20 @@ public class FluxTemplate
     public static string TemplateBlocos(Objeto obj)
     {
         return $$"""
-    Blocos --[
         blc.-id: {{obj.Id}}
         blc.-txt: {{obj.content}}
         blc.-x: {{obj.X}}
         blc.-y: {{obj.Y}}
         blc.-wdt: {{obj.Wdt}}
         blc.-hgth: {{obj.Hegt}}
-    ]
 """;
     }
 
     public static string TemplateLines(Linhas linha)
     {
         return $$"""
-    Conexão --[
         ln.-cnct_orgm: {{linha.OrigemId}}
         ln.-cnct_dstn: {{linha.DestinoId}}
-    ]    
 """;
     }
 }
